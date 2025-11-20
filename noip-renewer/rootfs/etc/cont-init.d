c7c4c5545c8b5443a00ce9b8ce0443c899657ba1
habuild <36288425+habuild@users.noreply.github.com>
2025-11-20 11:54:24 +1100

    Update cont-init.d

--- noip-renewer/rootfs/etc/cont-init.d
+++ noip-renewer/rootfs/etc/cont-init.d
@@ -5,7 +5,10 @@
 # s6-overlay docs: https://github.com/just-containers/s6-overlay
 # ==============================================================================
 
-  export EMAIL="$(jq --raw-output '.NOIP_Email' $CONFIG_PATH)"
-  export PASSWORD="$(jq --raw-output '.NOIP_Password' $CONFIG_PATH)"
-  export NOIP_TOTPKEY="$(jq --raw-output '.NOIP_TOTP_KEY' $CONFIG_PATH)"
-  export TRANSLATE="$(jq --raw-output '.Translate_Enabled' $CONFIG_PATH)"
+declare -a options
+  
+export Email="$(jq --raw-output '.NOIP_Email' $CONFIG_PATH)"
+export Password="$(jq --raw-output '.NOIP_Password' $CONFIG_PATH)"
+export NOIP_TOTP_KEY="$(jq --raw-output '.NOIP_TOTP_KEY' $CONFIG_PATH)"
+export Translate="$(jq --raw-output '.Translate_Enabled' $CONFIG_PATH)"
+
